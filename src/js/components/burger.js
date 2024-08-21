b23d95b36e0be07b646148ce6eae32be898e94d5
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2024-08-21 20:24:17 +0300

    add trick to transition from height 0 to auto

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,43 +1,43 @@
-import { disableScroll } from '../functions/disable-scroll.js'
-import { enableScroll } from '../functions/enable-scroll.js'
-;(function () {
-  const burger = document.querySelector('[data-burger]')
-  const menu = document.querySelector('[data-menu]')
-  const menuItems = document.querySelectorAll('[data-menu-item]')
-  const overlay = document.querySelector('[data-menu-overlay]')
+// import { disableScroll } from '../functions/disable-scroll.js'
+// import { enableScroll } from '../functions/enable-scroll.js'
+// ;(function () {
+//   const burger = document.querySelector('[data-burger]')
+//   const menu = document.querySelector('[data-menu]')
+//   const menuItems = document.querySelectorAll('[data-menu-item]')
+//   const overlay = document.querySelector('[data-menu-overlay]')
 
-  burger.addEventListener('click', (e) => {
-    burger.classList.toggle('header__burger--active')
-    menu.classList.toggle('header__nav--active')
-    overlay.classList.toggle('overlay--active')
+//   burger.addEventListener('click', (e) => {
+//     burger.classList.toggle('header__burger--active')
+//     menu.classList.toggle('header__nav--active')
+//     overlay.classList.toggle('overlay--active')
 
-    if (menu.classList.contains('header__nav--active')) {
-      burger.setAttribute('aria-expanded', 'true')
-      burger.setAttribute('aria-label', 'Close menu')
-      disableScroll()
-    } else {
-      burger.setAttribute('aria-expanded', 'false')
-      burger.setAttribute('aria-label', 'Open menu')
-      enableScroll()
-    }
-  })
+//     if (menu.classList.contains('header__nav--active')) {
+//       burger.setAttribute('aria-expanded', 'true')
+//       burger.setAttribute('aria-label', 'Close menu')
+//       disableScroll()
+//     } else {
+//       burger.setAttribute('aria-expanded', 'false')
+//       burger.setAttribute('aria-label', 'Open menu')
+//       enableScroll()
+//     }
+//   })
 
-  overlay.addEventListener('click', () => {
-    closeMenu()
-  })
+//   overlay.addEventListener('click', () => {
+//     closeMenu()
+//   })
 
-  menuItems.forEach((el) => {
-    el.addEventListener('click', () => {
-      closeMenu()
-    })
-  })
+//   menuItems.forEach((el) => {
+//     el.addEventListener('click', () => {
+//       closeMenu()
+//     })
+//   })
 
-  const closeMenu = () => {
-    burger.setAttribute('aria-expanded', 'false')
-    burger.setAttribute('aria-label', 'Open menu')
-    burger.classList.remove('header__burger--active')
-    menu.classList.remove('header__nav--active')
-    overlay.classList.remove('overlay--active')
-    enableScroll()
-  }
-})()
+//   const closeMenu = () => {
+//     burger.setAttribute('aria-expanded', 'false')
+//     burger.setAttribute('aria-label', 'Open menu')
+//     burger.classList.remove('header__burger--active')
+//     menu.classList.remove('header__nav--active')
+//     overlay.classList.remove('overlay--active')
+//     enableScroll()
+//   }
+// })()
