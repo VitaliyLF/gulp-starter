effa41accb571bd27d3706313ceebc8c914286d2
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2024-12-22 16:13:32 +0300

    update

--- src/js/components/dialog.js
+++ src/js/components/dialog.js
@@ -1,34 +1,3 @@
-// const dialogsElements = document.querySelectorAll('.js-dialog')
-// const openDialogsBtnsElements = document.querySelectorAll('.js-open-dialog')
-// const closeDialogsBtnsElements = document.querySelectorAll('.js-close-dialog')
-
-// const closeModalHandler = (dialog) => {
-//   dialog.close()
-// }
-
-// openDialogsBtnsElements.forEach((btn, index) => {
-//   btn.addEventListener('click', () => {
-//     dialogsElements[index].showModal()
-//   })
-// })
-
-// closeDialogsBtnsElements.forEach((btn, index) => {
-//   btn.addEventListener('click', () => {
-//     dialogsElements[index].close()
-//   })
-// })
-
-// dialogsElements.forEach((dialog) => {
-//   dialog.addEventListener('click', (event) => {
-//     const dialogModal = event.currentTarget
-//     const isClickedOnBackdrop = event.target === dialogModal
-
-//     if (isClickedOnBackdrop) {
-//       closeModalHandler(dialogModal)
-//     }
-//   })
-// })
-
 document.addEventListener('click', (event) => {
   const target = event.target
 
